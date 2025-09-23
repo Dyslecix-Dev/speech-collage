66e902102f483a1aec559c6bdc5f2b9a303adb7a
Christian Dean Demesa <dyslecixdev@gmail.com>
2025-09-22 20:21:22 -0700

    Added pictures

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,131 @@
+"use client";
+
 import { motion } from "motion/react";
 
 const RevealCards = () => {
   return (
-    <section className="p-8 bg-white">
+    <section className="p-8 bg-slate-800">
       <span className="block text-center text-xl font-medium mb-4">Hover a card</span>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-4xl mx-auto">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 max-w-7xl mx-auto">
         <Card
-          title="Build"
-          description="We make pretty buildings"
-          imgSrc="https://images.unsplash.com/photo-1488972685288-c3fd157d7c7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
-        />
-        <Card
-          title="See?"
-          description="This ones pretty nice"
-          imgSrc="https://images.unsplash.com/photo-1487958449943-2429e8be8625?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
-        />
-        <Card
-          title="TALL!"
-          description="We can even do TALL buildings"
-          imgSrc="https://images.unsplash.com/photo-1449157291145-7efd050a4d0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+          title="Alaska"
+          description="We make pretty buildings" // TODO
+          imgSrc="/alaska.png"
         />
 
         <Card
-          title="Wavy"
-          description="You like wavy buildings?"
-          imgSrc="https://images.unsplash.com/photo-1598818384697-62330d600309?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+          title="Anime"
+          description="This ones pretty nice" // TODO
+          imgSrc="/anime.png"
         />
+
         <Card
-          title="Modern"
-          description="This one's real blocky"
-          imgSrc="https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1160&q=80"
+          title="College Friends"
+          description="We can even do TALL buildings" // TODO
+          imgSrc="/college-friends.png"
         />
+
         <Card
-          title="Modular"
-          description="That is all :)"
-          imgSrc="https://images.unsplash.com/photo-1547282548-b82b40322759?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+          title="Drawing"
+          description="You like wavy buildings?" // TODO
+          imgSrc="/drawing.png"
+        />
+
+        <Card
+          title="Family"
+          description="This one's real blocky" // TODO
+          imgSrc="/family.png"
+        />
+
+        <Card
+          title="Graduation"
+          description="That is all :)" // TODO
+          imgSrc="/graduation.jpg"
+        />
+
+        <Card
+          title="High School Friends"
+          description="That is all :)" // TODO
+          imgSrc="/high-school-friends.png"
+        />
+
+        <Card
+          title="Hiking"
+          description="That is all :)" // TODO
+          imgSrc="/hike.png"
+        />
+
+        <Card
+          title="Lola"
+          description="That is all :)" // TODO
+          imgSrc="/lola.png"
+        />
+
+        <Card
+          title="New York"
+          description="That is all :)" // TODO
+          imgSrc="/new-york.png"
+        />
+
+        <Card
+          title="Other Close Friends"
+          description="That is all :)" // TODO
+          imgSrc="/other-friends.png"
+        />
+
+        <Card
+          title="Parents"
+          description="That is all :)" // TODO
+          imgSrc="/parents.png"
+        />
+
+        <Card
+          title="Playing Piano"
+          description="That is all :)" // TODO
+          imgSrc="/piano.png"
+        />
+
+        <Card
+          title="Museums"
+          description="That is all :)" // TODO
+          imgSrc="/pictures.png"
+        />
+
+        <Card
+          title="Science"
+          description="That is all :)" // TODO
+          imgSrc="/science.png"
+        />
+
+        <Card
+          title="Silly Picture"
+          description="That is all :)" // TODO
+          imgSrc="/silly.png"
+        />
+
+        <Card
+          title="Sky Diving"
+          description="That is all :)" // TODO
+          imgSrc="/sky-diving.png"
+        />
+
+        <Card
+          title="Software Development"
+          description="That is all :)" // TODO
+          imgSrc="/software-development.jpg"
+        />
+
+        <Card
+          title="Musicals"
+          description="That is all :)" // TODO
+          imgSrc="/theater.png"
+        />
+
+        <Card
+          title="Warner Bros. Studios"
+          description="That is all :)" // TODO
+          imgSrc="/warner-bros.png"
         />
       </div>
     </section>
@@ -45,9 +136,9 @@ const Card = ({ imgSrc, title, description }: { imgSrc: string; title: string; d
   return (
     <motion.div whileHover="hover" className="w-full h-[300px] relative">
       <div className="h-1/2 p-6 flex flex-col justify-center bg-black">
-        <h3 className="text-xl mb-2 font-semibold text-white">{title}</h3>
         <p className="text-sm font-light text-slate-300">{description}</p>
       </div>
+
       <motion.div
         initial={{
           top: "0%",
@@ -66,11 +157,10 @@ const Card = ({ imgSrc, title, description }: { imgSrc: string; title: string; d
           backgroundPosition: "center",
         }}
       />
-      <a href="#" rel="nofollow" className="w-1/2 h-1/2 absolute bottom-0 right-0 z-0 grid place-content-center bg-white text-black hover:text-indigo-500 transition-colors">
-        <div className="flex items-center">
-          <span className="text-xs">MORE</span>
-        </div>
-      </a>
+
+      <div className="w-1/2 h-1/2 absolute bottom-0 right-0 z-0 grid place-content-center bg-white text-black hover:text-indigo-500 transition-colors duration-300">
+        <h3 className="text-xl text-center mb-2 font-semibold">{title}</h3>
+      </div>
     </motion.div>
   );
 };
